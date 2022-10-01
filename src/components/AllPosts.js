5b63254c02b232dc52654f63e88fc25f11e810a3
Pub <96320073+b3Rhunter@users.noreply.github.com>
2022-10-01 16:49:31 -0500

    Add files via upload

--- src/components/AllPosts.js
+++ src/components/AllPosts.js
@@ -49,18 +49,18 @@ export default function AllPosts() {
                 allPostsData.map((post, index) => (
                   <Link to={"/" + post.slug.current} key={post.slug.current}>
                   <span
-                    className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 " style={{ borderColor: "#313131" }}
+                    className="block relative rounded shadow leading-snug bg-black border-l-8 " style={{ borderColor: "#07204a", height: "192px" }}
                     key={index}
                   >
                     <img
-                      className="w-full h-full rounded-r object-cover absolute"
+                      className="w-full h-full rounded-r object-contain absolute"
                       src={post.mainImage.asset.url}
                       alt=""
                       style={{paddingTop: "0px", marginTop: "0px"}}
                       />
                     <span className="block relative h-full flex justify-start items-start pr-4 pb-4">
 
-                      <h2 className="text-gray-800 text-lg font-bold px-3 py-3 text-red-100 flag" style={{ backgroundColor: "#313131" }}>
+                      <h2 className="text-gray-800 text-lg font-bold px-3 py-3 text-red-100 flag" style={{ backgroundColor: "rgba(0,0,0,0.8)" }}>
                         {post.title}
                       </h2>
 
